98779e2fa25d827838bd685964b30b19309e5454
ndiphiweNnombula <ndiphiwe.nombula@gmail.com>
2026-03-06 21:34:38 +0200

    feat(application): define sensitive word repository interface

---
+++ src/SensitiveWords.Application/Interfaces/ISensitiveWordRepository.cs
@@ -0,0 +1,15 @@
+﻿using SensitiveWords.Domain.Entities;
+
+namespace SensitiveWords.Application.Interfaces
+{
+    public interface ISensitiveWordRepository
+    {
+        Task<IEnumerable<SensitiveWord>> GetAllAsync();
+
+        Task<int> CreateAsync(string word);
+
+        Task UpdateAsync(int id, string word);
+
+        Task DeleteAsync(int id);
+    }
+}

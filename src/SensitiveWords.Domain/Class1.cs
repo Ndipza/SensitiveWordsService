f39b217fc9d56efbc3984be279d0e53e735d4490
ndiphiweNnombula <ndiphiwe.nombula@gmail.com>
2026-03-06 21:20:03 +0200

    feat(domain): add SensitiveWord entity

--- src/SensitiveWords.Domain/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SensitiveWords.Domain;
-
-public class Class1
-{
-
-}
---
+++ src/SensitiveWords.Domain/Entities/SensitiveWord.cs
@@ -0,0 +1,11 @@
+﻿namespace SensitiveWords.Domain.Entities
+{
+    public class SensitiveWord
+    {
+        public int Id { get; set; }
+
+        public string Word { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
